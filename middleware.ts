92423d3ec7ac3ecddfa259d7026c76eaa747ea46
TobiasGleiter <gleiter.tobias@outlook.de>
2023-08-26 11:18:58 +0200

    feat: added role authoization for admin dashboard

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,32 @@
-export { default } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(request) {
+    console.log(request.nextauth);
+    if (
+      request.nextUrl.pathname === '/admin-dashboard' &&
+      request.nextauth.token?.role !== 'admin'
+    ) {
+      return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: (params) => {
+        let { token } = params;
+        return !!token;
+      },
+    },
+  }
+);
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/equipment/:path*', '/auth/new-user'],
+  matcher: [
+    '/dashboard/:path*',
+    '/admin-dashboard/:path*',
+    '/equipment/:path*',
+    '/auth/new-user',
+  ],
 };

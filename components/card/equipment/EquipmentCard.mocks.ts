cbd6ae4fbe140208c92fb8ab23d51564e7267dad
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-09-05 08:46:13 +0200

    refactor: equipment update

--- components/card/equipment/EquipmentCard.mocks.ts
+++ components/card/equipment/EquipmentCard.mocks.ts
@@ -1,11 +1,8 @@
 import { IEquipmentCard } from './EquipmentCard';
 
 const base: IEquipmentCard = {
-  _id: 'any',
-  name: 'string',
-  category: '',
-  location: 'string',
-  session: 'any',
+  equipment: 'any',
+  session: null,
   handleDelete: 'any',
 };
 

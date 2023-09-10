8f8f6af05273f589ff75c941b2c92e5e70f6ca53
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-09-10 16:08:29 +0200

    refactor: dashboard

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,6 +1,13 @@
 import { authOptions } from '@/app/api/auth/[...nextauth]/options';
+import BaseIcon from '@/components/icons/base/BaseIcon';
 import { getServerSession } from 'next-auth';
 import Link from 'next/link';
+
+const nav = [
+  { label: 'Dashboard', href: '/dashboard', icon: 'dashboard' },
+  { label: 'Equipment', href: '/equipment', icon: 'equipment' },
+  { label: 'Sunday Service', href: '/sunday-service', icon: 'sundayservice' },
+];
 
 export default async function DashboardPage() {
   const session = await getServerSession(authOptions);
@@ -12,19 +19,19 @@ export default async function DashboardPage() {
       </div>
       <div className="YOUR_PAGES mt-6">
         <h1 className="text-3xl font-bold">Your pages</h1>
-        <div className="mt-2 grid grid-cols-3 gap-4">
-          <Link
-            href="/equipment"
-            className="border border-secondary-900 border-dashed p-2 rounded-lg text-center duration-200 hover:border-primary-500"
-          >
-            Equipment
-          </Link>
-          <Link
-            href="/sunday-service"
-            className="border border-secondary-900 border-dashed p-2 rounded-lg text-center duration-200 hover:border-primary-500"
-          >
-            Sunday Service
-          </Link>
+        <div className="mt-2 grid lg:grid-cols-3 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4">
+          {nav.map((item) => {
+            return (
+              <Link
+                key={item.label}
+                href={item.href}
+                className="bg-white h-[66px] py-1 px-4 rounded-2xl shadow-md w-full border border-white hover:border-primary-500"
+              >
+                <BaseIcon icon={item.icon} />
+                <p>{item.label}</p>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </div>
